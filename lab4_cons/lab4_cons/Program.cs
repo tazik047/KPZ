b452b891596f2d433e87ed73686e6f4f72699e98
Stas <stas249501@gmail.com>
2015-05-20 23:14:45 +0300

    lab 4

---
+++ lab4_cons/lab4_cons/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            while (s != "q")
+            {
+                File.WriteAllText("test.dat", s);
+                Scanner scanner = new Scanner("test.dat");
+                Parser parser = new Parser(scanner);
+                parser.Parse();
+                Console.Write(parser.errors.count + " errors detected" + Environment.NewLine);
+                s = Console.ReadLine();
+            }
+        }
+    }
+}
