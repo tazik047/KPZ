65b349ccf592a0410a0c4d22700069989119977e
Stas <stas249501@gmail.com>
2015-05-25 03:40:22 +0300

    last fix

--- lab4_cons/lab4_cons/Program.cs
+++ lab4_cons/lab4_cons/Program.cs
@@ -9,16 +9,24 @@ namespace lab4_cons
 {
     class Program
     {
+        static string TextName = "test.dat";
+
         static void Main(string[] args)
         {
             string s = Console.ReadLine();
             while (s != "q")
             {
-                File.WriteAllText("test.dat", s);
-                Scanner scanner = new Scanner("test.dat");
+                File.WriteAllText(TextName, s);
+                Token t;
+                Scanner scanner = new Scanner(TextName);
+                while ((t = scanner.Scan()).kind != 0)
+                {
+                    Console.WriteLine("\tToken:{0}, Lexeme {1}", t.kind, t.val);
+                }
+                scanner = new Scanner(TextName);
                 Parser parser = new Parser(scanner);
                 parser.Parse();
-                Console.Write(parser.errors.count + " errors detected" + Environment.NewLine);
+                Console.WriteLine(parser.errors.count + " errors detected");
                 s = Console.ReadLine();
             }
         }
